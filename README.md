188b4c8d705b47bb46e2aa8223088d376cf903ae
cheri marie <cherimarie@gmail.com>
2026-05-25 19:57:09 -0700

    Starter code for very slow loading data

---
+++ README.md
@@ -0,0 +1,11 @@
+# Week 9 Caching Demo
+
+This simple Express app is where we will practice server- and client-side caching.
+
+## Development
+
+This is an [Express application](https://expressjs.com/en/5x/api/) using (Pug templating engine](https://pugjs.org/api/reference.html). It uses [nodemon](https://github.com/remy/nodemon) for hot reload on file change, so no need to restart the server after changes.
+
+On clone, run `npm install`.
+
+Start the server with `npm run start`.
---
+++ routes/data.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const carData = require('../data/cars.json')
+
+// Add a 5 second delay to simulate a slow data fetch
+async function getDataSlowly(){
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/setTimeout
+  await new Promise(r=> setTimeout(r, 5000))
+  return carData
+}
+
+router.get('/', async function(req, res, next) {
+  const cars = await getDataSlowly();
+  res.json({data: cars });
+});
+
+module.exports = router;
